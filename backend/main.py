7c7b707b2efe8185b0d8c0022e7911902273a695
w1llx1m <williamsilva.monteiro28@gmail.com>
2026-07-23 09:45:28 -0300

    connecting backend with the frontend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return{"Data": "Test"}
+
+
+origins = [
+    "http://localhost:5173",  # dev do Vite
+    "https://montheon.com.br",  # produção, quando existir
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
